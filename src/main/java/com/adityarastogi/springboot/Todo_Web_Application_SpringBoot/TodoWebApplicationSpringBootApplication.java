3cb2846f368297b7a348227a648d44f87ddc2c4b
Aditya <adityarastogi620@gmail.com>
2025-02-09 05:14:39 +0530

    initial commit

---
+++ src/main/java/com/adityarastogi/springboot/Todo_Web_Application_SpringBoot/TodoWebApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.adityarastogi.springboot.Todo_Web_Application_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebApplicationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWebApplicationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adityarastogi/springboot/Todo_Web_Application_SpringBoot/TodoWebApplicationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityarastogi.springboot.Todo_Web_Application_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebApplicationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
